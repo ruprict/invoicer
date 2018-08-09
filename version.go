230a33316d19c20a05f42c29a3f25f02ac046930
Glenn Goodrich <glenn.goodrich@gmail.com>
2018-08-09 10:58:45 -0400

    Add ZAP Proxy docker step to circle ci config

--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
-const version = "20180614.0-92f47c3"
-const commit = "92f47c3d3dd24e41c8b38adbb86b2407143c9dc9"
+const version = "20180809.0-2fd326d"
+const commit = "2fd326d46ab15df09493525dfc62d2b72f837bd9"
